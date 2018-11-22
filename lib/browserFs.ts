4e0937c3c59452cccd4d1d6a14899ceb39871f9c
Viktor Westberg <v.festberg@gmail.com>
2018-11-22 21:14:32 +0100

    Convert all fs functions to use BrowserFs

---
+++ lib/browserFs.ts
@@ -0,0 +1,1 @@
+export let browserFs = null as any
--- lib/index.ts
+++ lib/index.ts
@@ -1,19 +1,24 @@
-"use strict";
 // Copyright 2013 Joyent, Inc.  All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-exports.__esModule = true;
-var browserFs = require("./browserFs");
+
+import * as browserFs from './browserFs'
+
 ///--- Helpers
+
 function _export(obj) {
     Object.keys(obj).forEach(function (k) {
         module.exports[k] = obj[k];
     });
 }
+
+
+
 ///--- Exports
-module.exports = { setBrowserFs: function (BFS) { browserFs.BFS = BFS; } };
+
+module.exports = { setBrowserFs: function(BFS) { (browserFs as any).BFS = BFS } };
+
 _export(require('./mount'));
 _export(require('./nfs'));
-//# sourceMappingURL=index.js.map
